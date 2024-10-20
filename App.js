ad5381e1079ba7201948a4db9b9eaa9b14c9fb5e
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-10-20 21:17:23 +0300

    added Login screen and form logic

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
 import { useFonts } from "expo-font";
+// import RegistrationScreen from "./Screens/RegistrationScreen";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,7 +17,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <RegistrationScreen />
+      <LoginScreen />
+      {/* <RegistrationScreen /> */}
       <StatusBar style="auto" />
     </View>
   );
--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -15,10 +15,8 @@ import {
 } from "react-native";
 import { commonStyles } from "../styles/common";
 
-const RegistrationScreen = () => {
+const LoginScreen = () => {
   const [secure, setSecure] = useState(true);
-  const [image, setImage] = useState(false);
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -31,30 +29,11 @@ const RegistrationScreen = () => {
           style={styles.backgroundImage}
         >
           <View style={styles.formWrapper}>
-            <View style={styles.imageWrapper}>
-              <Pressable onPress={() => setImage(!image)}>
-                <Image
-                  style={[styles.addButton, image && styles.rotate]}
-                  source={
-                    image
-                      ? require("../assets/delete.png")
-                      : require("../assets/add.png")
-                  }
-                />
-              </Pressable>
-            </View>
-            <Text style={[commonStyles.title, styles.title]}>Реєстрація</Text>
+            <Text style={[commonStyles.title, styles.title]}>Увійти</Text>
             <KeyboardAvoidingView
               style={styles.form}
               behavior={Platform.OS == "ios" ? "padding" : "height"}
             >
-              <TextInput
-                value={name}
-                onChangeText={setName}
-                placeholder="Логін"
-                style={commonStyles.input}
-                placeholderTextColor="#bdbdbd"
-              />
               <TextInput
                 value={email}
                 onChangeText={setEmail}
@@ -82,21 +61,18 @@ const RegistrationScreen = () => {
             <TouchableOpacity
               style={[commonStyles.button, styles.button]}
               onPress={() => {
-                console.log(
-                  `Логін: ${name}, електронна пошта: ${email}, пароль:${password}`
-                );
-                setName("");
+                console.log(`Електронна пошта: ${email}, пароль: ${password}`);
                 setEmail("");
                 setPassword("");
               }}
             >
-              <Text style={commonStyles.buttonText}>Зареєструватися</Text>
+              <Text style={commonStyles.buttonText}>Увійти</Text>
             </TouchableOpacity>
             <View style={commonStyles.accentTextWrapper}>
-              <Text style={commonStyles.accentText}>Вже є акаунт?</Text>
+              <Text style={commonStyles.accentText}>Немає акаунту?</Text>
               <Pressable>
                 <Text style={[commonStyles.accentText, styles.link]}>
-                  Увійти
+                  Зареєструватися
                 </Text>
               </Pressable>
             </View>
@@ -116,32 +92,12 @@ const styles = StyleSheet.create({
   formWrapper: {
     marginTop: "auto",
     backgroundColor: "#fff",
-    paddingBottom: 45,
+    paddingBottom: 111,
     paddingRight: 16,
     paddingLeft: 16,
-    paddingTop: 92,
+    paddingTop: 32,
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-  },
-  imageWrapper: {
-    top: -60,
-    left: "50%",
-    transform: [{ translateX: -50 }],
-    position: "absolute",
-    width: 120,
-    height: 120,
-    borderRadius: 16,
-    backgroundColor: "#f6f6f6",
-  },
-  addButton: {
-    position: "absolute",
-    top: 80,
-    right: -12,
-    width: 25,
-    height: 25,
-  },
-  rotate: {
-    transform: [{ rotate: "-45deg" }],
   },
   title: {
     marginBottom: 32,
@@ -167,4 +123,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default RegistrationScreen;
+export default LoginScreen;
